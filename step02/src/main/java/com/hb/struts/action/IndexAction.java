44ec9cc33c768e9b8044ee97caca9b510c385176
1027 <1027@hb6000>
2017-05-31 17:19:30 +0900

    struts2-maven project

---
+++ step02/src/main/java/com/hb/struts/action/IndexAction.java
@@ -0,0 +1,9 @@
+package com.hb.struts.action;
+
+public class IndexAction {
+
+	public String execute() {
+		// TODO Auto-generated method stub
+		return "success";
+	}
+}
---
+++ step02/src/main/webapp/index.jsp
@@ -0,0 +1,6 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+<a href="list.action">입장</a>
+</body>
+</html>
