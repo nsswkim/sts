2b599a128861a49bb1608c48a1deaefa19dfc6a5
1027 <1027@hb6000>
2017-06-02 12:34:14 +0900

    spring-starter day01

---
+++ sts01/src/com/hb/day01/controller/IndexController.java
@@ -0,0 +1,19 @@
+package com.hb.day01.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class IndexController implements Controller {
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest req, HttpServletResponse res) throws Exception {
+		System.out.println("indexController ½ÇÇà");
+		ModelAndView mav=new ModelAndView();
+		mav.setViewName("main");
+		return mav;
+	}
+
+}
